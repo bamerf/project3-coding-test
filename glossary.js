e351d6bc504c17a21674a67925e4c702bee81a0c
Dave <oldmcdonald1@gmail.com>
2019-05-21 15:40:20 +1000

    glossary with basic scroll

---
+++ glossary.js
@@ -0,0 +1,26 @@
+// dom elements
+const sidebarLetters = document.querySelectorAll('.glossary-sidebar li');
+const definitions = document.querySelectorAll('.definition-list li');
+
+
+
+// functions
+const toggleDetail = event => {
+  console.log('clicked');
+  // collapse all details
+  
+  // make target title black
+  event.target.style.color = "black";
+  
+  // expand details of target
+  const detail = event.target.parentNode.querySelector('article')
+  detail.classList.toggle('hidden')
+}
+
+
+
+
+// event listeners
+definitions.forEach(definition => {
+  definition.addEventListener('click', toggleDetail)
+})
