c671717de8bce69fb0b7ab7567b5ba99991e6a60
bam-erfanian <erfanianbamdad@gmail.com>
2019-05-21 11:15:49 +1000

    fixed database and added 5 seeds

--- schema.sql
+++ schema.sql
@@ -1,7 +1,15 @@
+CREATE DATABASE project3;
+
 CREATE TABLE words(
-  id serial primary key,
+  id SERIAL PRIMARY KEY,
   word VARCHAR(300),
   description VARCHAR(300),
-  hints TEXT[],
-  tags TEXT[]
+  hints TEXT [],
+  url VARCHAR(300)
+);
+
+CREATE TABLE tags(
+  id SERIAL PRIMARY KEY,
+  words_id INTEGER NOT NULL,
+  tag VARCHAR(300)
 );
---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO words(word, description, hints, url) VALUES ('CSS', 'Adds styles to web pages', ARRAY ['Style', 'Selectors', 'Layout'], 'https://en.wikipedia.org/wiki/Cascading_Style_Sheets');
+
+INSERT INTO words(word, description, hints, url) VALUES ('HTML', 'Its the language of websites', ARRAY ['Tags', 'Boilerplate', 'Semantic'], 'https://en.wikipedia.org/wiki/HTML');
+
+INSERT INTO words(word, description, hints, url) VALUES ('HTTP', 'An application protocol for distributed, collaborative, hypermedia information systems', ARRAY ['Route', 'Method', 'Request'], 'https://en.wikipedia.org/wiki/Hypertext_Transfer_Protocol');
+
+INSERT INTO words(word, description, hints, url) VALUES ('JSON', 'an open-standard file format that uses human-readable text to transmit data objects consisting of attribute–value pairs', ARRAY ['Object', 'API Request', 'JavaScript'], 'https://en.wikipedia.org/wiki/JSON');
+
+INSERT INTO words(word, description, hints, url) VALUES ('REPL', 'is a simple, interactive computer programming environment that takes single user inputs, evaluates them, and returns the result to the user', ARRAY ['Pry', 'Console', 'Terminal'], 'https://en.wikipedia.org/wiki/Read%E2%80%93eval%E2%80%93print_loop');
