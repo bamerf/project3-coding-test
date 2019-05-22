d3541b632445bbb3f82d7b399f9e695f38efa037
Dave <oldmcdonald1@gmail.com>
2019-05-22 17:46:51 +1000

    more server stuff all good

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const express = require ('express');
+
+// start up server
+const express = require('express');
 const app = express();
 const port = 8080;
-
-
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
 })
-
 
 // pg for interfacing to database
 const { Client } = require('pg')
@@ -14,11 +13,35 @@ const client = new Client({
   database: 'project3'
 })
 
-client.connect()
+// set the view directory to ./pages
+app.set("views", `./pages`)
 
-client.query('SELECT * FROM words', [], (err, res) => {
-  res.rows.forEach(row => console.log(row.word))
-  client.end()
+// express setting to use ejs as render engine
+app.set('view engine', 'ejs')
+
+
+
+
+
+
+// routes
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+  res.render('index', { message: 'im mister meeseeks'});
 })
 
 
+
+app.get('/allwords', (req, res) => {
+  // database query
+  client.connect()
+  client.query('SELECT * FROM words', [], (err, dbresponse) => {
+    // dbresponse.rows.forEach(row => console.log(row.word))
+
+    res.send(dbresponse.rows);
+
+    client.end()
+  })
+  
+})
