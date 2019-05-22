b8e8ceda910695f5b3a3e1ff3b746738b84dcd07
Dave <oldmcdonald1@gmail.com>
2019-05-22 19:36:45 +1000

    backend routes working with express and ejs. thanks my lord

--- public/javascript/glossary.js
+++ public/javascript/glossary.js
@@ -18,36 +18,9 @@ const collapsibleDetail = event => {
   }
 }
 
-const makeNewListItem = () => {
-
-}
-
-
-const getDatabaseItems = () => {
-
-}
-
 
 
 // event listeners
 definitionItem.forEach(definition => {
   definition.addEventListener('click', collapsibleDetail)
 })
-
-
-
-
-
-
-/*
-            <li>
-              <a href="#" class="definition-title">CRUD</a>
-              <article class="definition-details hidden">
-                  <p>In computer programming, create, read, update, and delete (CRUD) are the four basic functions of persistent storage. Alternate words are sometimes used when defining the four basic functions of CRUD, such as retrieve instead of read, modify instead of update, or destroy instead of delete. CRUD is also sometimes used to describe user interface conventions that facilitate viewing, searching, and changing information; often using computer-based forms and reports.</p>
-                  <footer>
-                    <a href="#"><i class="fas fa-tag tag-icon"></i>html</a>
-                    <a href="#"><i class="fas fa-tag tag-icon"></i>javascript</a>
-                  </footer>
-                </article>
-            </li>
-*/
