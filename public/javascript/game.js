294b60d9d2438a675a7de0a8b05c2ca0b2039091
bam-erfanian <erfanianbamdad@gmail.com>
2019-05-23 17:34:17 +1000

    game funtionality

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -1,10 +1,5 @@
 winCount = 0
 words = null
-guessWord = "something"
-hint1Text = "dsdsd"
-hint2Text = "sdffv"
-hint3Text = "fdsfdbf"
-description = "ldjfdkhjf"
 
 //jQuery wait for page load
 $(document).ready(function() {
@@ -19,83 +14,67 @@ $(document).ready(function() {
     function handleDone(res) {
       words = res.words;
     }
-  
+    
     $.ajax(options).done(handleDone)
-  
+    
   }
-
+  
   getWords()
   // $(window).on('load', getWords)
-
-
-  // Game start
-  $('.start-btn').on('click', gameStart)
   
-
   const gameStart = () => {
-
+    
+    $(this).fadeOut("fast")
+    
     for(let i = 0; i < 10; i++) {
+      
+      // Change input size base on number of words
+      if (words[i].includes(" ")) {
+        $('.input').css({height: '100px'})
+      }
       
       // Indicate number of characters
       $('.num-char').text(words[i].length)
       $('.input').keyup(function() {
         $('.num-char').text(words[i].length - $('.input').val().length)
       })
+
+      //Hints
+      $('.hint1').children('.hint-inner').children
+      ('.hint-back').text(words[i].hint1)
+      $('.hint2').children('.hint-inner').children('.hint-back').text(words[i].hint2)
+      $('.hint3').children('.hint-inner').children('.hint-back').text(words[i].hint3)
+      $('.hint1').on('click', function(e) {
+        e.target.closest('.hint1').classList.add('reveal')
+      })
+      
+      //Correct Answer
+      $('.input').keyup(function() {
+        if (words[i] === $('.input').val()) {
+          $('.input').css({outline: "solid green"})
+          
+          winCount += 1;
+          
+        }
+      })
+      
+      // Hints Events
+      $('.hint1').on('click', function(e) {
+        e.target.closest('.hint1').classList.add('reveal')
+      })
+      
+      $('.hint2').on('click', function(e) {
+        e.target.closest('.hint2').classList.add('reveal')
+      })
+      
+      $('.hint3').on('click', function(e) {
+        e.target.closest('.hint3').classList.add('reveal')
+      })
     }
-
-
-
   }
   
+  // Game start
+  $('.start-btn').on('click', gameStart)
 
-
-
-
-
-  //Correct Answer
-  $('.input').keyup(function() {
-    if (guessWord === $('.input').val()) {
-      $('.input').css({outline: "solid green"})
-
-      winCount += 1;
-
-    }
-  })
-
-  //Hints
-  $('.hint1').children('.hint-inner').children
-  ('.hint-back').text(hint1Text)
-  $('.hint2').children('.hint-inner').children('.hint-back').text(hint2Text)
-  $('.hint3').children('.hint-inner').children('.hint-back').text(hint3Text)
-  $('.hint1').on('click', function(e) {
-    e.target.closest('.hint1').classList.add('reveal')
-  })
-
-  // Hints Events
-  $('.hint1').on('click', function(e) {
-    e.target.closest('.hint1').classList.add('reveal')
-  })
-  // Green for Correct Answer & Description
-  $('.input').keyup(function() {
-    if (guessWord === $('.input').val()) {
-      $('.input').css({outline: "solid green"})
-    }
-  })
-  
-  $('.hint2').on('click', function(e) {
-    e.target.closest('.hint2').classList.add('reveal')
-  })
-
-  $('.hint3').on('click', function(e) {
-    e.target.closest('.hint3').classList.add('reveal')
-  })
-
-  $('.start-btn').on('click', function() {
-    $(this).fadeOut("fast")
-  })
 
 })
-
-if (guessWord.includes(" ")) {
-  $('.input').css({height: '100px'})
-}
