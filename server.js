a0246762a2a0ee44726f66aaca4243562ebebd69
Dave <oldmcdonald1@gmail.com>
2019-05-24 12:19:50 +1000

    update port to work on production

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const _ = require('lodash')
 // read more: https://expressjs.com/
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`listening on port: ${port}`)
 })
